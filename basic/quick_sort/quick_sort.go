d0cb729f136335be8c630e25516508a976a1fc76
john.zhang@shopee.com <john.zhang@shopee.com>
2023-08-18 17:51:32 +0800

    improve quick sort

--- basic/quick_sort/quick_sort.go
+++ basic/quick_sort/quick_sort.go
@@ -2,24 +2,40 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 func main() {
-	//a := []int{1, 2, 3, 4, 5, 6, 4, 7, 7}
-	//rand.Shuffle(len(a), func(i, j int) {
-	//	a[i], a[j] = a[j], a[i]
-	//})
-	a := []int{5, 7, 6}
+	a := []int{1, 2, 3, 4, 5, 6, 4, 7, 7}
+	rand.Shuffle(len(a), func(i, j int) {
+		a[i], a[j] = a[j], a[i]
+	})
+	//a := []int{5, 7, 6}
 	fmt.Println(a)
 
 	QuickSort(a, 0, len(a)-1)
 
 	fmt.Println(a)
 }
+
+const CUTOFF = 10
+
 func QuickSort(a []int, lo, hi int) {
 	if lo >= hi {
 		return
 	}
+
+	// 优化：insertion sort
+	if hi <= lo+CUTOFF-1 {
+		InsertionSort(a, lo, hi)
+		return
+	}
+
+	// 优化: 使用中间元素
+	// ~ 12/7 n ln n compares (14% fewer)
+	// ~ 12/35 n ln n exchanges (3% more)
+	median := medianOf3(a, lo, lo+(hi-lo)/2, hi)
+	swap(a, lo, median)
 
 	// partition
 	midIdx := partition(a, lo, hi)
@@ -27,6 +43,48 @@ func QuickSort(a []int, lo, hi int) {
 	// sort left, right
 	QuickSort(a, lo, midIdx-1) // 这里必须+1和-1，否则会只剩最后2个元素时，导致无穷递归
 	QuickSort(a, midIdx+1, hi)
+}
+
+func InsertionSort(a []int, lo int, hi int) {
+	if lo == hi {
+		return
+	}
+
+	for i := lo; i <= hi; i++ {
+		// find smallest of the rest
+		minIdx := i
+		for j := i + 1; j <= hi; j++ {
+			if less(a[j], a[minIdx]) {
+				minIdx = j
+			}
+		}
+
+		// swap the min to i position
+		swap(a, i, minIdx)
+	}
+}
+
+func less(a, b int) bool {
+	return a < b
+}
+func medianOf3(a []int, lo int, i int, hi int) int {
+	if a[lo] < a[hi] {
+		if a[i] < a[lo] {
+			return lo
+		} else if a[i] > a[hi] {
+			return hi
+		} else {
+			return i
+		}
+	} else {
+		if a[i] > a[lo] {
+			return lo
+		} else if a[i] < a[hi] {
+			return hi
+		} else {
+			return i
+		}
+	}
 }
 
 func partition(a []int, lo, hi int) int {
