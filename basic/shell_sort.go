3b9cbd3f472414270b7d988a6d91437725e02879
john.zhang@shopee.com <john.zhang@shopee.com>
2023-08-22 19:03:36 +0800

    shuffle

---
+++ basic/shell_sort.go
@@ -0,0 +1,8 @@
+package basic
+
+// Idea. Move entries more than one position at a time by h-sorting the array.
+// 每次对比-移动更大距离的的两个元素。
+//How to h-sort an array? Insertion sort, with stride length h.
+func shellSort(a []int) {
+
+}
