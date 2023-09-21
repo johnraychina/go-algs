053c49e9ea4108204d1af7d94aca343c604312af
john.zhang@shopee.com <john.zhang@shopee.com>
2023-09-21 19:03:10 +0800

    shortest path

---
+++ graph/shortest_path.go
@@ -0,0 +1,18 @@
+package graph
+
+// 问题抽象：给定一个有权重的有向图，请找到s点到t点的最短路径。
+// Given an edge-weighted digraph, find the shortest path from s to t.
+
+//・PERT/CPM. 项目管理，关键路径。
+//・Map routing. 地图寻路
+//・Seam carving. 图像伸缩
+//・Robot navigation. 机器人导航
+//・Texture mapping. 着色 https://zhuanlan.zhihu.com/p/453457104
+//・Typesetting in TeX. TeX排版
+//・Urban traffic planning. 城市交通规划
+//・Optimal pipelining of VLSI chip. VLSI(Very Large Scale Integration)超大规模集成芯片最优流水线设计
+//・Telemarketer operator scheduling.
+//・Routing of telecommunications messages.
+//・Network routing protocols (OSPF, BGP, RIP).
+//・Exploiting arbitrage opportunities in currency exchange.
+//・Optimal truck routing through given traffic congestion pattern.
